4b0ac42e75fa3d6b387e771144d655d1ed0c0722
siongesteban <siong.esteban@gmail.com>
2021-05-17 17:30:59 +0800

    Install storybook-dark-mode

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,18 @@
+import { themes } from '@storybook/theming';
+import '!style-loader!css-loader!../src/components/app/app.css';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  darkMode: {
+    stylePreview: true,
+    classTarget: 'body',
+    darkClass: 'dark',
+    lightClass: 'light',
+  },
+};
