52ba18e286cd1493e6caa951eedb0c6bd4d9144d
siongesteban <siong.esteban@gmail.com>
2021-05-17 23:18:44 +0800

    Update Table to accept field alignment

--- src/common/components/table/table.component.tsx
+++ src/common/components/table/table.component.tsx
@@ -5,6 +5,7 @@ import styles from './table.module.css';
 export type TableField<TRow> = {
   name?: keyof TRow;
   text?: string;
+  alignment?: 'left' | 'center' | 'right';
   render?: (row: TRow) => React.ReactNode;
 };
 
@@ -18,9 +19,15 @@ export const Table = <TRow extends Record<string, any>>({
   rows,
 }: TableProps<TRow>) => {
   const renderHead = () => {
-    const items = fields.map(({ name, text }, i) => (
-      <th key={name?.toString() || i}>{text}</th>
-    ));
+    const items = fields.map(({ name, text, alignment = 'left' }, i) => {
+      const className = styles[`align-${alignment}`];
+
+      return (
+        <th key={name?.toString() || i} className={className}>
+          {text}
+        </th>
+      );
+    });
 
     return (
       <thead>
@@ -32,10 +39,18 @@ export const Table = <TRow extends Record<string, any>>({
   const renderBody = () => {
     const items = rows.map((row, i) => (
       <tr key={i}>
-        {fields.map(({ name, render }, i) => {
-          const content = render?.(row) || (name ? row[name] : null);
+        {fields.map(({ name, render, alignment = 'left' }, i) => {
+          const className = styles[`align-${alignment}`];
 
-          return <td key={i}>{content}</td>;
+          const customContent = render?.(row);
+          const value = name ? row[name] : null;
+          const content = customContent || value;
+
+          return (
+            <td key={i} className={className}>
+              {content}
+            </td>
+          );
         })}
       </tr>
     ));
