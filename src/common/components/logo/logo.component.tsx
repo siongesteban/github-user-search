c465f5a9b295b1b4c044fadf9b2fde91d4eae01d
siongesteban <siong.esteban@gmail.com>
2021-05-18 02:59:37 +0800

    Update class name in Logo

--- src/common/components/logo/logo.component.tsx
+++ src/common/components/logo/logo.component.tsx
@@ -6,7 +6,7 @@ export const Logo: React.FC = () => (
   <div className={styles.wrapper}>
     <h1 className={styles.logo}>
       <span className={styles.github}>GitHub</span>
-      <span className={styles.user}>Usearch</span>
+      <span className={styles.search}>Search</span>
     </h1>
     <span className={styles.slogan}>Search GitHub users or organizations</span>
   </div>
